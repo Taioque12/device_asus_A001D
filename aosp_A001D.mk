22f4d802b40a6a7dc4fd4a8bca8c0168d901344f
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-04 17:07:54 -0300

    Update aosp_A001D.mk

--- aosp_A001D.mk
+++ aosp_A001D.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/A001D/device.mk)
 
 # Inherit common Pixelexperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 
