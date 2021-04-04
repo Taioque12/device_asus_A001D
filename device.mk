ca0cb493a3d9e385c04ce872bc9a8c709b78bb96
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-04 11:29:35 -0300

    Update display

--- device.mk
+++ device.mk
@@ -173,7 +173,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
@@ -183,19 +182,11 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
+    libgenlock \
     liboverlay \
     libqdMetaData.system \
-    libgenlock \
+    libvulkan \
     libtinyxml
-
-PRODUCT_PACKAGES += \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
-	
-# Display Calibrators
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/display/qdcm_calib_data_ili9881h_720p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881h_720p_video_mode_dsi_panel.xml \
-    $(LOCAL_PATH)/configs/display/qdcm_calib_data_inx6301_720p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_inx6301_720p_video_mode_dsi_panel.xml
 
 # DRM
 PRODUCT_PACKAGES += \
