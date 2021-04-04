ed8bc0f6ec9c5b5ed62cb1ae9a57d5b5ce4656aa
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-04 01:42:55 -0300

    Update overlay

--- device.mk
+++ device.mk
@@ -29,7 +29,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
