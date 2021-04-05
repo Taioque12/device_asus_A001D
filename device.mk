fadda8d6ddef2e1af64048cfbe6099817eff68dd
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-05 12:49:16 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -154,13 +154,10 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.6 \
     camera.msm8953 \
-    libfui \
-    libui_shim \
     Snap \
     camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0
