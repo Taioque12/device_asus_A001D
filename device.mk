cf669814a22654d25bf4e72b00116433b73eb731
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-07 11:51:12 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -315,12 +315,6 @@ PRODUCT_COPY_FILES += \
 # Properties
 -include device/asus/A001D/prop.mk
 
-# Vulkan
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
