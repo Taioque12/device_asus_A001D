144aeb1a2883277aeeefe91bb711f595da85f83d
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-09 12:15:21 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -153,14 +153,17 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.6 \
+    camera.msm8953 \
+    libfui \
+    libui_shim \
     Snap \
-		camera.msm8953 \
-		libmm-qcamera \
-		android.hardware.camera.provider@2.4-impl \
-		android.hardware.camera.provider@2.4-service \
-		android.hardware.camera.provider@2.6 \
-		android.hardware.camera.device@3.6 \
-		vendor.qti.hardware.camera.device@1.0
+    camera.device@3.2-impl \
+    vendor.qti.hardware.camera.device@1.0
 
 # Component overrides
 PRODUCT_COPY_FILES += \
