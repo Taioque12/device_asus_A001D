087e000d6d20da1316dc6bcb3d343b9bf5e40802
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-12 23:11:10 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -186,22 +186,21 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     copybit.msm8953 \
     gralloc.msm8953 \
-    hwcomposer.msm8953 \
-    memtrack.msm8953 \
-    libdisplayconfig \
-    liboverlay \
-    libqdMetaData.system \
-    libgenlock \
-    libtinyxml
-
-PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
+    android.hardware.memtrack@1.0-service \
+    android.hardware.configstore@1.0-service \
+    hwcomposer.msm8953 \
+    memtrack.msm8953 \
+    libgenlock \
+    libtinyxml \
+    libdisplayconfig \
+    libqdMetaData.system \
+    libcutils_shim \
 
 PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
